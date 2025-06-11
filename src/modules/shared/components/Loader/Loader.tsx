deaaf80fc0f805c2907d2d54afe10345262e6db2
Zubkovskiy Bogdan <zubkovvsbogdan@gmail.com>
2025-06-11 23:10:41 +0300

    imrove Loader and Nesting

--- src/modules/shared/components/Loader/Loader.tsx
+++ src/modules/shared/components/Loader/Loader.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
 import styles from './Loader.module.scss';
 
 export const Loader = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setVisible(true);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (!visible) {
+    return null;
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.loading_wave}>
